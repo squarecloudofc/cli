bc02d2422b2ed813478aa7a5829c482868dbf58a
Richard Martins <me@richaar.dev>
2024-01-25 02:50:52 -0300

    load config file

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -4,15 +4,24 @@ import (
 	"io"
 	"os"
 
+	"github.com/squarecloudofc/cli/internal/config"
 	"github.com/squarecloudofc/cli/internal/rest"
 )
 
 type SquareCli struct {
-	Rest *rest.RestClient
+	Rest   *rest.RestClient
+	Config *config.Config
 }
 
 func NewSquareCli() (squareCli *SquareCli) {
-	squareCli = &SquareCli{}
+	config, err := config.Load()
+	if err != nil {
+		panic("could not load config file")
+	}
+
+	squareCli = &SquareCli{
+		Config: config,
+	}
 
 	return
 }
