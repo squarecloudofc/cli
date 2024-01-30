511b8aca47ef872da1a5ef0a14dbe926abda7574
Richard Martins <me@richaar.dev>
2024-01-30 03:24:16 -0300

    update build informations

--- internal/build/version.go
+++ internal/build/version.go
@@ -1,7 +1,5 @@
 package build
 
-import "time"
-
-var BuildTime = time.Now().UTC().Format(time.RFC3339)
 var Version = "unknown"
-var GitCommit = "unknown"
+var Commit = "unknown"
+var CommitDate = "unknown"
