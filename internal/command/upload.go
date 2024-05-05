adacba53f73d7773b858150aaf532d7240c69a26
Richard Martins <me@richaar.dev>
2024-05-05 17:56:33 -0300

    chore: make upload command be able to upload file already zipped

--- internal/command/upload.go
+++ internal/command/upload.go
@@ -14,73 +14,99 @@ import (
 	"github.com/squarecloudofc/cli/pkg/zipper"
 )
 
+type UploadOptions struct {
+	File       string
+	ConfigFile *squareconfig.SquareConfig
+}
+
 func NewUploadCommand(squareCli *cli.SquareCli) *cobra.Command {
+	options := UploadOptions{}
+
 	cmd := &cobra.Command{
 		Use:   "upload",
 		Short: "Upload your application to Square Cloud",
-		RunE:  runUploadCommand(squareCli),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			config, er := squareconfig.Load()
+			if er != nil {
+				return er
+			}
+
+			options.ConfigFile = config
+
+			return runUploadCommand(squareCli, &options)
+		},
 	}
 
-	cmd.PersistentFlags().BoolP("restart", "r", false, "Restart your application when commit")
+	cmd.Flags().StringVar(&options.File, "file", "", "File you want to upload to square cloud")
 	// TODO: Future
 	//  cmd.PersistentFlags().StringP("file", "f", "", "Zip of the application you want to commit")
 	return cmd
 }
 
-func runUploadCommand(squareCli *cli.SquareCli) RunEFunc {
-	return func(cmd *cobra.Command, args []string) (err error) {
-		rest := squareCli.Rest()
+func runUploadCommand(squareCli *cli.SquareCli, options *UploadOptions) error {
+	rest := squareCli.Rest()
 
-		var appId string
-		config, er := squareconfig.Load()
-		if er != nil {
-			return er
-		}
+	var appId string
 
-		if config.IsCreated() {
-			fmt.Fprintln(squareCli.Out(), "Seems you don't have a squarecloud.app config file, create one.")
-			return
-		}
-
-		workDir, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-
-		zipfilename := path.Join(workDir, "*.zip")
-		file, err := os.CreateTemp("", filepath.Base(zipfilename))
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		defer os.Remove(file.Name())
-
-		ignoreFiles, err := squareignore.Load()
-		if err != nil {
-			ignoreFiles = []string{}
-		}
-
-		err = zipper.ZipFolder(workDir, file, ignoreFiles)
-		if err != nil {
-			return err
-		}
-
-		success, err := rest.ApplicationUpload(appId, file.Name())
-		if err != nil {
-			return err
-		}
-
-		if success.ID != "" {
-			config.ID = success.ID
-			err := config.Save()
-
-			fmt.Fprintf(squareCli.Out(), "%s Your application has been uploaded\n", ui.CheckMark)
-			if err != nil {
-				fmt.Fprint(squareCli.Out(), "Unable to save your application id into squarecloud.app config file\n")
-			}
-		} else {
-			fmt.Fprintf(squareCli.Out(), "%s Unable to commit your application\n", ui.XMark)
-		}
-		return nil
+	workDir, err := os.Getwd()
+	if err != nil {
+		return err
 	}
+
+	var file *os.File
+	if options.File != "" {
+		file, err = os.Open(filepath.Join(workDir, options.File))
+		if err != nil {
+			fmt.Fprintln(squareCli.Out(), "Unable to open the zip file")
+			return err
+		}
+	} else {
+		file, err = zipWorkdir(workDir)
+		if err != nil {
+			fmt.Fprintln(squareCli.Out(), "Unable to zip the working directory")
+			return err
+		}
+	}
+
+	success, err := rest.ApplicationUpload(appId, file.Name())
+	if err != nil {
+		return err
+	}
+
+	if success.ID != "" {
+		if !options.ConfigFile.IsCreated() {
+			options.ConfigFile.ID = success.ID
+			err = options.ConfigFile.Save()
+		}
+
+		fmt.Fprintf(squareCli.Out(), "%s Your application has been uploaded\n", ui.CheckMark)
+		if err != nil {
+			fmt.Fprint(squareCli.Out(), "Unable to save your application id into squarecloud.app config file\n")
+		}
+	} else {
+		fmt.Fprintf(squareCli.Out(), "%s Unable to commit your application\n", ui.XMark)
+	}
+	return nil
+}
+
+func zipWorkdir(wd string) (*os.File, error) {
+	zipfilename := path.Join(wd, "*.zip")
+	file, err := os.CreateTemp("", filepath.Base(zipfilename))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	defer os.Remove(file.Name())
+
+	ignoreFiles, err := squareignore.Load()
+	if err != nil {
+		ignoreFiles = []string{}
+	}
+
+	err = zipper.ZipFolder(wd, file, ignoreFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
 }
